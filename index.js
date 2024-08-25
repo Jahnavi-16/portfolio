195c60fa0e66ec0ee42f1c128cb9361fd05576da
jahnavivajinapelli@gmail.com <jahnavivajinapelli2004@gmail.com>
2024-08-26 02:14:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".text", {
+    strings: ["Web Developer", "Software Developer"],
+    typeSpeed: 100, // default type speed
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
